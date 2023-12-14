b8c00c216bd6143a60256bf640b4022c7232ba17
Adrián Insaurralde Avalos <adrianinsaval@gmail.com>
2023-12-14 02:47:06 -0300

    fix installing opendark on windows

--- conda/win/create_bundle.bat
+++ conda/win/create_bundle.bat
@@ -30,8 +30,8 @@ git clone https://github.com/Ondsel-Development/Ondsel-Lens.git %conda_env%\Libr
 git -C %conda_env%\Library\Mod\Ondsel-Lens rev-parse HEAD
 
 echo "Install OpenDark preference pack"
-git clone https://github.com/obelisk79/OpenDark %conda_env%\Library\share\Gui\PreferencePacks\OpenDark
-git -C %conda_env%\Library\share\Gui\PreferencePacks\OpenDark rev-parse HEAD
+git clone https://github.com/obelisk79/OpenDark %conda_env%\Library\data\Gui\PreferencePacks\OpenDark
+git -C %conda_env%\Library\data\Gui\PreferencePacks\OpenDark rev-parse HEAD
 
 REM Copy Conda's Python and (U)CRT to FreeCAD/bin
 robocopy %conda_env%\DLLs %copy_dir%\bin\DLLs /S /MT:%NUMBER_OF_PROCESSORS% > nul
