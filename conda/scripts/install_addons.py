f47a743e684c80b38f15aee8a606227bcf9cc354
Adrián Insaurralde Avalos <adrianinsaval@gmail.com>
2024-01-15 16:16:19 -0300

    improve addon installation script

--- conda/scripts/install_addons.py
+++ conda/scripts/install_addons.py
@@ -4,8 +4,8 @@ import platform
 import sys
 import freecad
 import FreeCAD as App
-import AddonManager
 from addonmanager_installer import AddonInstaller
+import Addon
 
 conda_env=os.path.join(os.getcwd(),sys.argv[1])
 if platform.system() != 'Windows':
@@ -15,22 +15,22 @@ else:
     modspath = os.path.join(conda_env,'Library','Mod')
     prefpackspath = os.path.join(conda_env,'Library','data','Gui','PreferencePacks')
 
-# params = App.ParamGet("User parameter:BaseApp/Preferences/Addons")
-# params.SetBool('disableGit', True)
+params = App.ParamGet("User parameter:BaseApp/Preferences/Addons")
+params.SetBool('disableGit', True)
 
-def install_mod(name,url,installation_path = modspath):
-    installer = AddonInstaller(AddonManager.Addon(name,url))
+def install_mod(name,url,branch,installation_path = modspath):
+    installer = AddonInstaller(Addon.Addon(name,url,branch=branch))
     installer.installation_path = installation_path
     installer.run()
 
-install_mod('Ondsel-FirstRun','https://github.com/Ondsel-Development/Ondsel-FirstRun')
+install_mod('Ondsel-FirstRun','https://github.com/Ondsel-Development/Ondsel-FirstRun','main')
 
 mods_json = os.path.join(modspath,'Ondsel-FirstRun','mods.json')
 with open(mods_json, "r") as fp:
     mods = json.load(fp)
 
 for mod in mods["addons"]:
-    install_mod(name=mod["name"],url=mod["url"])
+    install_mod(mod["name"],mod["url"],mod['branch'])
 
 for mod in mods["preferencePacks"]:
-    install_mod(name=mod["name"],url=mod["url"],installation_path=prefpackspath)
+    install_mod(mod["name"],mod["url"],mod['branch'],prefpackspath)
